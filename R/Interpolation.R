2ef796a798f94fcc8fcf003d8bc3b88765cc0159
LydiaYW <yingjia.wei@hsc.utah.edu>
2024-12-09 11:14:41 -0700

    updates

--- R/Interpolation.R
+++ R/Interpolation.R
@@ -7,20 +7,9 @@
 #' @examples
 #' # Here is an example
 #'
-PredictNormal <- function(dt, exam="Mesopic", model="LMM"){
-  # check arguments
-  if(!(model %in% c("LMM", "BQR", "RF"))){
-    stop("Model opitions: LMM, BQR, RF")
-  }
-
+Interpolation <- function(dt, exam){
   if(!(exam %in% c("Mesopic", "Cyan", "Red"))){
     stop("Please indicate the type of exam (Mesopic, Cyan or Red)")
   }
-
-  # check data format
-  if(!is.character(dt[,1])){
-    stop("Please include ID in first column")
-  }
-
 
 }
